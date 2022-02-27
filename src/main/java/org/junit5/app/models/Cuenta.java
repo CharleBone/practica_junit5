ec72aa9231b675d5c67ddf59acef6e297a034d91
Charle <carlos.itbone@gmail.com>
2022-02-27 02:10:44 -0300

    Test TDD referencia Cuenta

--- src/main/java/org/junit5/app/models/Cuenta.java
+++ src/main/java/org/junit5/app/models/Cuenta.java
@@ -31,4 +31,20 @@ public class Cuenta implements Serializable {
     public void setSueldo(BigDecimal sueldo) {
         this.sueldo = sueldo;
     }
+
+    public void debito(BigDecimal monto) {
+        this.sueldo.subtract(monto);
+    }
+
+    public void credito(BigDecimal monto) {
+        this.sueldo.add(monto);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (!(obj instanceof Cuenta)) return false;
+        if (this.persona == null || this.sueldo == null) return  false;
+        Cuenta c = (Cuenta) obj;
+        return this.persona.equals(c.getPersona()) && this.sueldo.equals(c.getSueldo());
+    }
 }
